dd00039ce1d2a5e711b722b8289d311654e95e40
teoma145 <matteodark145@yahoo.it>
2023-10-13 17:27:12 +0200

    array-tail-fix-if

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,15 +1,29 @@
-let arraynumeri=[]
 let btn= document.getElementById('mybutton')
+let textel=document.getElementById('data')
 
 btn.addEventListener('click',function(){
-    let uservalue =document.getElementById('elementi').value;
-    
+    let uservalue =parseInt(document.getElementById('elementi').value);
+    let stampvalue =parseInt(document.getElementById('stampare').value);
+    let arraynumeri=[]
+    textel.innerHTML=``
     for(let x=0; x < uservalue;x++){
         let numerogenerato = Math.floor(getRandomNumber(1, 100));
-        let arraynumeri=[]
         arraynumeri.push(numerogenerato)
         console.log(arraynumeri)
     }
+    if(stampvalue >= uservalue){
+        textel.innerHTML=`Il numero di elementi da scegliere deve essere più grande di quello da stampare`
+    }
+  
+     else if(stampvalue >= 0){
+        let lastElements = arraynumeri.slice(-stampvalue);
+        textel.innerHTML=`${lastElements}`
+    }
+    else if (stampvalue === '' || stampvalue === '0' || isNaN(stampvalue)) {
+        let lastElements = arraynumeri.slice(-5);
+        textel.innerHTML=`${lastElements}`
+    }
+    
 })
 
 
