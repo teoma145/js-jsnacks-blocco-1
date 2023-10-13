d3516ae3fce08764121053328005edf02c62916e
teoma145 <matteodark145@yahoo.it>
2023-10-13 17:37:50 +0200

    2arrays

---
+++ array_bonus/js/script.js
@@ -0,0 +1,19 @@
+let array1 =[
+    'uno,',
+    'due',
+    'tre',
+    'quattro',
+    'cinque',
+    'sei',
+    'sette',
+    'otto',
+]
+let array2 =[
+    'nove',
+    'dieci',
+    'unidici',
+    'dodici',
+]
+for(x=0;array2.length > array1.length;x++){
+    
+}
